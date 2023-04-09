cbc6c4c3f53c8e341159f9d2db4171bba5ac7b61
Coerus <77758986+coerus@users.noreply.github.com>
2023-04-08 21:30:46 -0300

    Add files via upload

---
+++ curso-go/aula2/ex1.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"curso-go/aula2/math"
+)
+
+func main() {
+	resultado := math.Somax(1)
+	fmt.Printf("%v \n", resultado)
+}
---
+++ curso-go/aula2/math/opx.go
@@ -0,0 +1,6 @@
+
+package math
+
+func Somax(a int) int {
+	return a + 10
+}
